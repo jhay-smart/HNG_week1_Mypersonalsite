42b3807b13fe7b1f6597b23b0991505c0ef30d4a
jhay <omalejoy83@gmail.com>
2024-07-01 11:05:25 +0100

    basic structure designed and styled

---
+++ script.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    const now = new Date();
+    const day = now.toLocaleString("en-US", { weekday: "long", month: "short", day: "numeric", year: "numeric" });
+    const time = now.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric", hour12: true, timeZoneName: "short" });
+    document.querySelector("[data-testid='currentDay']").innerText = day;
+    document.querySelector("[data-testid='currentTimeUTC']").innerText = time;
+  }, 1000); 
